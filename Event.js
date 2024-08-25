3e0c042837913aed77c5e254970e868bcf2e80e9
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2024-08-25 15:52:32 +0530

    Update Event.js

--- Event.js
+++ Event.js
@@ -1,34 +1,34 @@
-// // function fun1(){
-// //     process.nextTick(()=>console.log('For Fun1()')) //  console.log('For Fun1()') is wrapped in a callback function that is passed to process.nextTick(). This means that as soon as the current JavaScript stack is clear, the event loop will pick up the callback and execute it, outputting "For Fun1()" to the console.
-// //     setTimeout(()=>console.log('This is for fun1() makes it asynchrounous'),2000)   // this will work asynchronously like at the last 
-// //     setImmediate(()=>console.log('This is a set immediate'))
-// // }
+function fun1(){
+    process.nextTick(()=>console.log('For Fun1()')) //  console.log('For Fun1()') is wrapped in a callback function that is passed to process.nextTick(). This means that as soon as the current JavaScript stack is clear, the event loop will pick up the callback and execute it, outputting "For Fun1()" to the console.
+    setTimeout(()=>console.log('This is for fun1() makes it asynchrounous'),2000)   // this will work asynchronously like at the last 
+    setImmediate(()=>console.log('This is a set immediate'))
+}
 
-// // function fun2(){
-// //     console.log('This is function 2');
+function fun2(){
+    console.log('This is function 2');
     
-// // }
+}
 
-// // function fun3(){
-// //     console.log('This is function 3');
-// // }
+function fun3(){
+    console.log('This is function 3');
+}
 
-// // fun1()
-// // fun2()
-// // fun3()
+fun1()
+fun2()
+fun3()
 
 
-// setTimeout(() =>{
-//     console.log('This is a set TimeOut !!!');
-// },3000)
+setTimeout(() =>{
+    console.log('This is a set TimeOut !!!');
+},3000)
 
-// const fs = require('fs')
-// fs.readFile(__filename,() =>{
-//     console.log('I am file')
-// })
+const fs = require('fs')
+fs.readFile(__filename,() =>{
+    console.log('I am file')
+})
 
-// process.nextTick(()=> console.log('I am a process'))     // this is all a callback function
-// // Promise.resolve().then(()=> console.log('This is a Promise')) // this is a micro task firstly execute
+process.nextTick(()=> console.log('I am a process'))     // this is all a callback function
+// Promise.resolve().then(()=> console.log('This is a Promise')) // this is a micro task firstly execute
 
 
 
