b19a61f357c82462842b3e495c211c8c80dd8e2e
Aryat <aryatsrishadow007@gmail.com>
2025-02-17 03:11:23 +0530

    Implemented Rest API 🎗️

---
+++ api/restAPI.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const movies = require('./MOCK_DATA (1).json');
+
+app.get('/', (req, res) => {
+    res.send('<h1>Learning Rest API 🔗</h1>');
+});
+
+app.get('/api/movies', (req, res) => {
+   return res.json(movies);
+});
+
+app.get('/movies',(req,res)=>{
+    const html = 
+    `<h3>Movies</h3>
+    <ul>
+        ${movies.map(movie=>`<li>${movie.movie_titles}</li>`).join('')}
+    </ul>
+    `
+    res.send(html);
+})
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000 ');
+});
+
